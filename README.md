a2469690430fa78054a27a22fa1e733be5f7c3b2
cvsgbhn <kbh123456g@gmail.com>
2020-09-29 22:02:51 +0300

    now can show all tasks in a simple way

---
+++ README.md
@@ -0,0 +1,8 @@
+initial run
+`go run todo/todo.go`
+
+add task
+`go run todo/todo.go add "task text"`
+
+show all tasks
+`go run todo/todo.go ls`
--- todo/todo.go
+++ todo/todo.go
@@ -6,14 +6,16 @@ import (
   "os"
   "time"
   "encoding/json"
-  "io/ioutil"
+  "bufio"
 
   "github.com/urfave/cli/v2"
 )
-// 1. записать в файл
-// 2. отметить как выполненное или проваленное
-// 3. добавить сохранение даты выполнения/провала
-// 4. выводить на экран красивенько все задачи
+// --- 1. записать в файл ---  DoNe
+// --- 2. выводить на экран красивенько все задачи --- DoNe
+// 2.1. форматировать дату нормально
+// 3. отметить как выполненное или проваленное
+// 4. добавить сохранение даты выполнения/провала
+// 4. добавить причину выполнения/провала (тэги?) и как можно улучшить выполнение в следующий раз
 
 type Task struct {
   Text string
@@ -22,9 +24,28 @@ type Task struct {
   End_date string
 }
 
-func save_task() {
+func ShowAllTasks() {
+  // Open tasks.json in read-mode.
+  file, _ := os.Open("todo/tasks.json")
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    textFromFile := scanner.Text()
+    task := Task{}
+    err := json.Unmarshal([]byte(textFromFile), &task)
+    if err != nil {
+      fmt.Println(err)
+    }
+    fmt.Println(task.Start_date, "  ", task.Text, "  ", task.Status)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "shouldn't see an error scanning a string")
+	}
+
+}
+
+func SaveTask(taskText string) {
   var new_task Task
-  new_task.Text = "new task in process now"
+  new_task.Text = taskText
   new_task.Status = "in process"
   t := time.Now()
   new_task.Start_date = t.String()
@@ -33,10 +54,14 @@ func save_task() {
     fmt.Println("error occured")
   }
   fmt.Println(string(b))
-  buf := []byte(b)
-  err = ioutil.WriteFile("./taskline", buf, 0644)
-  if err != nil {
-    fmt.Println("error occured")
+  // Open tasks.json in append-mode.
+  f, _ := os.OpenFile("todo/tasks.json", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+  // Append our json to tasks.json
+  if _, err = f.Write(b); err != nil {
+    panic(err)
+  }
+  if _, err = f.Write([]byte("\n")); err != nil {
+    panic(err)
   }
 }
 
@@ -51,9 +76,15 @@ func main() {
         name = c.Args().Get(0)
       }
       if name == "add" {
-        save_task()
+        SaveTask(c.Args().Get(1))
+      } 
+      if name == "ls" {
+        ShowAllTasks()
+      } else {
+        fmt.Println("master of this dungeon counts 10")
       }
-    },
+      return nil
+      },
   }
 
   err := app.Run(os.Args)
