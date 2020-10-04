da1885e316a49a1094b0e464b23044813508605a
cvsgbhn <kbh123456g@gmail.com>
2020-10-04 21:01:35 +0300

    can change tsk status now

--- todo/todo.go
+++ todo/todo.go
@@ -10,24 +10,32 @@ import (
 
   "github.com/urfave/cli/v2"
 )
-// --- 1. записать в файл ---  DoNe
-// --- 2. выводить на экран красивенько все задачи --- DoNe
-// 2.1. форматировать дату нормально
-// 3. отметить как выполненное или проваленное
+// --- 1. записать в файл ---  Done
+// --- 2. выводить на экран красивенько все задачи --- Done
+// --- 2.1. форматировать дату нормально --- Done
+// 3. отметить как выполненное или проваленное: прочитать файл, записать всё в список структур, найти нужную, изменить, перезаписать всё скопом в файл 
 // 4. добавить сохранение даты выполнения/провала
 // 4. добавить причину выполнения/провала (тэги?) и как можно улучшить выполнение в следующий раз
+// 5. Id ???
 
 type Task struct {
+  Id int
   Text string
   Status string
   Start_date string
   End_date string
+  WhatHelped string
+  WhatMessedUp string
 }
 
-func ShowAllTasks() {
-  // Open tasks.json in read-mode.
+func UpdateStatus()  {
+  /*
+  ** First step: reading all tasks to a slice of Task structures
+  */
   file, _ := os.Open("todo/tasks.json")
   scanner := bufio.NewScanner(file)
+  taskList := []Task{}
+  
   for scanner.Scan() {
     textFromFile := scanner.Text()
     task := Task{}
@@ -35,7 +43,56 @@ func ShowAllTasks() {
     if err != nil {
       fmt.Println(err)
     }
-    fmt.Println(task.Start_date, "  ", task.Text, "  ", task.Status)
+    taskList = append(taskList, task)
+    fmt.Println(task.Status)
+  }
+  fmt.Println("Count tasks:")
+  fmt.Println(len(taskList))
+
+  /*
+  ** Second step: rewrite status in any structure
+  */
+  taskList[7].Status = "Success"
+
+  /*
+  ** Third step: rewrite the whole file
+  */
+  err := os.Remove("todo/tasks.json")
+  if err != nil {
+    return
+  }
+  for _, task := range taskList {
+    b, err := json.Marshal(task)
+    if err != nil {
+      fmt.Println("error occured")
+    }
+    fmt.Println(string(b))
+    // Open tasks.json in append-mode.
+    f, _ := os.OpenFile("todo/tasks.json", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+    // Append our json to tasks.json
+    if _, err = f.Write(b); err != nil {
+      panic(err)
+    }
+    if _, err = f.Write([]byte("\n")); err != nil {
+      panic(err)
+    }
+  }
+}
+
+func ShowAllTasks() {
+  // Open tasks.json in read-mode.
+  file, _ := os.Open("todo/tasks.json")
+  scanner := bufio.NewScanner(file)
+  num := 0
+  for scanner.Scan() {
+    num++
+    textFromFile := scanner.Text()
+    task := Task{}
+    err := json.Unmarshal([]byte(textFromFile), &task)
+    if err != nil {
+      fmt.Println(err)
+    }
+    fmt.Println(num, "  ", task.Start_date, "  ", task.Text, "  ", task.Status)
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "shouldn't see an error scanning a string")
@@ -45,10 +102,13 @@ func ShowAllTasks() {
 
 func SaveTask(taskText string) {
   var new_task Task
+  new_task.Id = 1
   new_task.Text = taskText
   new_task.Status = "in process"
-  t := time.Now()
-  new_task.Start_date = t.String()
+  t := time.Now().Format("02-01-2006")
+  fmt.Println(t)
+  //new_task.Start_date = t.String()
+  new_task.Start_date = t
   b, err := json.Marshal(new_task)
   if err != nil {
     fmt.Println("error occured")
@@ -77,10 +137,11 @@ func main() {
       }
       if name == "add" {
         SaveTask(c.Args().Get(1))
-      } 
-      if name == "ls" {
+      } else if name == "ls" {
         ShowAllTasks()
-      } else {
+      } else if name == "us" {
+        UpdateStatus()
+      }else {
         fmt.Println("master of this dungeon counts 10")
       }
       return nil
